554947bc3bad93f82b0db4daa0e9b3443ec1ec95
Vernik22 <vernik22.ing@gmail.com>
2024-06-11 22:59:22 -0600

    BackEnd
    inicio Backend

---
+++ Proyecto1/Backend/Controller/DataController.go
@@ -0,0 +1,20 @@
+package Controller
+
+import (
+	"Backend/Instance"
+	"Backend/Model"
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func InsertData(c *fiber.Ctx, nameCol string, dataParam string ) error  {
+	collection := Instance.Mg.Db.Collection(nameCol)
+	doc:= Model.Data{Percent: dataParam}
+
+	_, err:= collection.InsertOne(context.TODO(), doc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.Status(201).JSON(dataParam)
+}
---
+++ Proyecto1/Backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"Backend/Database"
+	"Backend/Routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
+)
+
+func main()  {
+	app := fiber.New()
+
+	if err := Database.Connect(); err != nil {
+		log.Fatal("Error en ",err)
+	}
+
+	app.Use(cors.New())
+
+	Routes.Setup(app)
+
+	if err:= app.Listen(":8000"); err != nil{
+		panic(err)
+	}
+}
